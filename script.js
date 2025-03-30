55fa9c93fe85e86f8364a48070194cb23361cd64
Mladen kitic <mladenkitic1993@gmail.com>
2025-03-30 18:41:49 +0200

    second

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle the navbar menu on mobile
-const hamburger = document.getElementById('hamburger');
-const navbarList = document.querySelector('.navbar-list');
-
-hamburger.addEventListener('click', () => {
-    navbarList.classList.toggle('active');
-});
